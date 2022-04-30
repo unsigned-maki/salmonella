9cb70973d6cedb3697762c9e76604f8d592e540d
Niclas Nell <nel20326@spengergasse.at>
2022-05-01 00:02:27 +0200

    style(poll controller): format code

--- controllers/poll.py
+++ controllers/poll.py
@@ -5,26 +5,31 @@ def create_poll(author, options, title, description):
         return False
 
     insert_options=[]
-    for option in options: 
+
+    for option in options:
         insert_options.append(db.models.Option(text=option)) 
 
-    try: 
+    try:
         new_poll = db.models.Poll(author=author, options=insert_options, title=title, description=description)
         new_poll.save() 
-    except ValidationError: 
+    except ValidationError:
         return False
 
     return new_poll.id
 
-def get_poll(**kwargs): 
+def get_poll(**kwargs):
     polls = db.models.Poll.objects(**kwargs)
-    if not polls.count(): 
-        return False
-    return polls[0]
 
-def delete_poll(id): 
-    polls=polls.models.Poll.objects (id=id)
     if not polls.count():
         return False
+
+    return polls[0]
+
+def delete_poll(id):
+    polls=polls.models.Poll.objects(id=id)
+
+    if not polls.count():
+        return False
+
     polls[0].delete()
     return True
