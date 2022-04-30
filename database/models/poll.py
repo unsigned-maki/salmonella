537dc8cdda7c175fb65b41ca2e0911586facaf3d
Niclas Nell <nel20326@spengergasse.at>
2022-04-30 22:49:22 +0200

    feat(poll model): add description and options field

--- database/models/poll.py
+++ database/models/poll.py
@@ -1,13 +1,20 @@
 import uuid
 from .user import User
+from .option import Option
 from mongoengine import *
 
 def valid_author(author):
     if not User.objects(id=author).count():
         raise ValidationError("Author is not a valid user")
 
+def valid_options(options):
+    if len(options) < 2:
+        raise ValidationError("Minimum of 2 options required")
+
 class Poll(Document):
     id = UUIDField(default=uuid.uuid4(), primary_key=True)
     title = StringField(required=True)
+    description = StringField(required=True)
     author = UUIDField(required=True, validation=valid_author)
+    options = EmbeddedDocumentListField(Option, required=True, validation=valid_options)
     
