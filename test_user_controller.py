aed20a97f5c30a11663d847d3aae000ba6fb36bd
Niclas Nell <nel20326@spengergasse.at>
2022-05-25 04:22:02 +0200

    test(user controller): add test for new str to UUID conversion

--- test_user_controller.py
+++ test_user_controller.py
@@ -4,35 +4,54 @@ from controllers import user
 
 fake = Faker()
 
+
 def test_create_user_a():
     assert user.create_user(fake.name(), fake.password())
+
 
 def test_create_user_b():
     name = fake.name()
     user.create_user(name, fake.password())
     assert not user.create_user(name, fake.password())
 
+
 def test_get_user_a():
     new_user = user.create_user(fake.name(), fake.password())
     assert user.get_user(id=new_user)
 
+
 def test_get_user_b():
     assert not user.get_user(id=uuid.uuid4())
+
 
 def test_get_user_c():
     name = fake.name()
     new_user = user.create_user(name, fake.password())
     assert user.get_user(name=name)
 
+
 def test_get_user_d():
     assert not user.get_user(name=fake.name())
+
+
+def test_get_user_e():
+    new_user = user.create_user(fake.name(), fake.password())
+    assert user.get_user(id=new_user)
+
+
+def test_get_user_f():
+    new_user = user.create_user(fake.name(), fake.password())
+    assert user.get_user(id=str(new_user))
+
 
 def test_delete_user_a():
     new_user = user.create_user(fake.name(), fake.password())
     assert user.delete_user(new_user)
 
+
 def test_delete_user_b():
     assert not user.delete_user(uuid.uuid4())
+
 
 def test_update_user_password_a():
     new_user = user.create_user(fake.name(), fake.password())
