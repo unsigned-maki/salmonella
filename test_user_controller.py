0286dc1ab894de71a2c48e6853f9b9b7d5f4687e
Niclas Nell <nel20326@spengergasse.at>
2022-05-27 19:46:06 +0200

    test(user controller): refactor tests to work with new create_user paramater

--- test_user_controller.py
+++ test_user_controller.py
@@ -8,18 +8,18 @@ fake = Faker()
 
 
 def test_create_user_a():
-    assert user.create_user(fake.name(), fake.password())
+    assert user.create_user(fake.name(), pw := fake.password(), pw)
 
 
 def test_create_user_b():
     name = fake.name()
-    user.create_user(name, fake.password())
+    user.create_user(name, pw := fake.password(), pw)
     with pytest.raises(ValidationError, match=f"User {name} already exists."):
-        user.create_user(name, fake.password())
+        user.create_user(name, pw := fake.password(), pw)
 
 
 def test_get_user_a():
-    new_user = user.create_user(fake.name(), fake.password())
+    new_user = user.create_user(fake.name(), pw := fake.password(), pw)
     assert user.get_user(id=new_user)
 
 
@@ -29,7 +29,7 @@ def test_get_user_b():
 
 def test_get_user_c():
     name = fake.name()
-    new_user = user.create_user(name, fake.password())
+    new_user = user.create_user(name, pw := fake.password(), pw)
     assert user.get_user(name=name)
 
 
@@ -38,17 +38,17 @@ def test_get_user_d():
 
 
 def test_get_user_e():
-    new_user = user.create_user(fake.name(), fake.password())
+    new_user = user.create_user(fake.name(), pw := fake.password(), pw)
     assert user.get_user(id=new_user)
 
 
 def test_get_user_f():
-    new_user = user.create_user(fake.name(), fake.password())
+    new_user = user.create_user(fake.name(), pw := fake.password(), pw)
     assert user.get_user(id=str(new_user))
 
 
 def test_delete_user_a():
-    new_user = user.create_user(fake.name(), fake.password())
+    new_user = user.create_user(fake.name(), pw := fake.password(), pw)
     assert user.delete_user(new_user)
 
 
@@ -57,7 +57,7 @@ def test_delete_user_b():
 
 
 def test_update_user_password_a():
-    new_user = user.create_user(fake.name(), fake.password())
+    new_user = user.create_user(fake.name(), pw := fake.password(), pw)
     password = user.get_user(id=new_user).password
     user.update_user_password(new_user, fake.password())
     assert user.get_user(id=new_user).password != password
