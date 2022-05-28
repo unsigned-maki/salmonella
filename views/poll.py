29f7bedc8e5ad56997308f7a702b63fd1f293b62
Niclas Nell <nel20326@spengergasse.at>
2022-05-28 17:55:27 +0200

    feat(poll views): add vote view

--- views/poll.py
+++ views/poll.py
@@ -1,29 +1,69 @@
 import sse
+import uuid
+import controllers.user as user
 import controllers.poll as controller
 from secure.auth import auth
 from .message import Message
-from jinja2 import TemplateNotFound
 from mongoengine import ValidationError
-from flask import Blueprint, render_template, abort, request, session, Response
+from flask import Blueprint, render_template, request, session, Response, abort
 
 poll = Blueprint('poll', __name__, template_folder='templates')
 
 
 @poll.route("/view")
 def view_all():
-    try:
-        return render_template("poll.html")
-    except TemplateNotFound:
-        abort(404)
+    pl = controller.get_poll(title="Meins")
+
+    i = ""
+
+    for option in pl.options:
+        i += str(option.id) + " --- "
+
+    #return str(pl.options[0].id)
+    return pl.to_json()
 
 
-@poll.route("/view/<id>", methods=["GET"])
-def view_one(id):
-    controller.get_poll(id)
-    try:
-        return render_template("poll.html")
-    except TemplateNotFound:
-        abort(404)
+@poll.route("/vote/<id>", methods=["GET", "POST"])
+def vote(id):
+    if not (pl := controller.get_poll(id=id)):
+        abort(404)  # not found
+
+    author = user.get_user(id=pl.author)
+
+    if request.method == "POST":
+        if auth.is_authenticated(session) and author:
+            if author.id == auth.get_user(session).id:
+                return render_template(
+                    "vote.html",
+                    poll=pl,
+                    author=author.name if author else "Deleted User",
+                    message=Message("error", "You must not vote on your own poll."))
+
+        if len(request.form) != 1:
+            return render_template(
+                "vote.html",
+                poll=pl,
+                author=author.name if author else "Deleted User",
+                message=Message("warning", "No option was selected."))
+
+        choice = list(request.form)[0]
+
+        for option in pl.options:
+            if uuid.UUID(choice) == option.id:
+                option.votes += 1
+                pl.save()
+                return render_template("index.html")
+
+        return render_template(
+            "vote.html",
+            poll=pl,
+            author=author.name if author else "Deleted User",
+            message=Message("error", "Something went wrong selecting your option."))
+    else:
+        return render_template(
+            "vote.html",
+            poll=pl,
+            author=author.name if author else "Deleted User")
 
 
 @poll.route("/create", methods=["GET", "POST"])
