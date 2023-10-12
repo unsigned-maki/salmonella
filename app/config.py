132b4e7f621a1871b7da6ef8b2543ca9d1057af1
Niclas Nell <nel20326@spengergasse.at>
2023-10-13 01:32:41 +0200

    refactor(config): remove obsolete cache config

--- app/config.py
+++ app/config.py
@@ -1,9 +1,3 @@
-CACHE_CONFIG = {
-    "DEBUG": True,
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
-
 REQUIRE_AUTHENTICATION = [
     "user.change_password",
     "poll.create",
