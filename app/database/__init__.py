994cd9e687b1802cb0ccd9c20f7b6e1ad081410f
Niclas Nell <nel20326@spengergasse.at>
2023-10-13 13:04:49 +0200

    fix(database): fix mongodb connection string error

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,6 +2,9 @@ import os
 from . import models
 from mongoengine import *
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 
-connect(DB_HOST)
+connect(db=DB_NAME, username=DB_USER, password=DB_PASS, host=DB_HOST)
